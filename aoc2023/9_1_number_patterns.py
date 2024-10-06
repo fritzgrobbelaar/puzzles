b50ee822d917c82b4fe939eae05033bb49475aa8
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2024-10-06 16:56:41 +0100

    9_1 success

--- aoc2023/9_1_number_patterns.py
+++ aoc2023/9_1_number_patterns.py
@@ -1,5 +1,5 @@
 from lib import cleaninput
-from lib.number_pattern import extrapolatePattern
+from lib.number_pattern import calculateTotalFromInput
 
 listOfText_Puzzle = cleaninput.getRowsOfNumbersLists('aoc2023\\input9.txt')
 
@@ -8,24 +8,9 @@ listOfText_Sample='''0 3 6 9 12 15
 10 13 16 21 30 45'''.split('\n')
 listOfText_Sample = cleaninput.convertRowsOfTextToRowsOfNumbers(listOfText_Sample)
 
-listOfText = listOfText_Sample
+#listOfText = listOfText_Sample
+listOfText = listOfText_Puzzle
 
-print(listOfText)
+print('total: ', calculateTotalFromInput(listOfText))
 
 exit()
-
-for row in text:
-    print(row)
-    info = row.split(':')[1]
-    left, right = info.split('|')
-    numbers1= set(left.split())
-    addRowWinnings = 0
-    for number in right.split():
-        if number in numbers1:
-            print('found', number)
-            if not addRowWinnings:
-                addRowWinnings = 1
-            else:
-                addRowWinnings = addRowWinnings*2
-    total = total+addRowWinnings
-print('total:', total)
--- lib/number_pattern.py
+++ lib/number_pattern.py
@@ -1,7 +1,7 @@
 def buildNumberPyramid(row):
     allRows = [row]
     while set(allRows[-1]) != set([0]):
-        print('allRows [-1]', allRows[-1])
+ #       print('allRows [-1]', allRows[-1])
         row = allRows[-1]
         newRow = []
         for i, value in enumerate(row[1:]):
@@ -11,14 +11,20 @@ def buildNumberPyramid(row):
 
 def extrapolatePattern(row):
     pyramid = buildNumberPyramid(row)
-    print("\n\nprocessing:\n", pyramid)
+    #print("\n\nprocessing:\n", pyramid)
     pyramid.reverse()
     for i, row in enumerate(pyramid):
         if i == 0:
             newNumber = 0
         else:
-            print('calculate ', row[-1] , ' plus ', pyramid[i-1][-1])
+     #       print('calculate ', row[-1] , ' plus ', pyramid[i-1][-1])
             newNumber = row[-1] + pyramid[i-1][-1]
-        print("adding new number:", newNumber)
+      #  print("adding new number:", newNumber)
         pyramid[i].append(newNumber)
     return newNumber
+
+def calculateTotalFromInput(input):
+    total = 0
+    for row in input:
+        total += extrapolatePattern(row)
+    return total
