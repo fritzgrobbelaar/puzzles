d5af65bfc284a6c77e1505853796254b2be7393f
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2024-10-09 20:28:10 +0100

    Started with recurssion and remaining string

--- aoc2023/12_2_binary_counter.py
+++ aoc2023/12_2_binary_counter.py
@@ -41,10 +41,6 @@ def doesIdsMatch(binaryString, ids):
         pass
     return False
 
-def countUnknowns(row):
-    count = row.count('?')
-    print("count of unknowns", count,' in ', row)
-    return count
 
 assert 3 == countUnknowns('...??#?##')
 assert 4 == countUnknowns('.?..??#?##')
@@ -63,36 +59,34 @@ def replaceWithBinary(row, binaryString):
 assert '000001111' == replaceWithBinary('000??1?11', '001')
 assert '0100101111' == replaceWithBinary('0?00??1?11', '1101')
 
-def calculateBinaryStringsFromIds(row, ids):
-    for train in ids.split(','):
 
-    return 10
     
 
 
-def calculateCombinations(row):
-    row, ids = row.split(' ')
-    row = row*2
-    print('processing row', row)
-    ids = ','.join([ids]*2)
+def calculateRemainingCombinations(remainingRow, remainingIds):
+    print('processing row', remainingRow, remainingIds)
+
     total = 0
     import itertools
-    row = row.replace('.','0').replace('#','1')
-    #for binaryString in map(''.join, itertools.product('01', repeat=countUnknowns(row))):
-    for binaryString in calculateBinaryStringsFromIds(row, ids)
+    remainingRow = remainingRow.replace('.','0').replace('#','1')
+    remainingIds = ids
 
-        binaryString = replaceWithBinary(row, binaryString)
-        if doesIdsMatch(binaryString, ids):   
-            if doesStringMatch(binaryString, row):
-                print('match: ', binaryString, row, ids)
-                total+=1
+
+    # for binaryString in map(''.join, itertools.product('01', repeat=countUnknowns(row))):
+    #     binaryString = replaceWithBinary(row, binaryString)
+    #     if doesIdsMatch(binaryString, ids):
+    #         if doesStringMatch(binaryString, row):
+    #             print('match: ', binaryString, row, ids)
+    #             total+=1
     return total
 
 finalTotal = 0
 
 for i,row in enumerate(rows):
-    
-    result = calculateCombinations(row)
+    row, ids = row.split(' ')
+    row = row*2
+    ids = ids = ','.join([ids]*2)
+    result = calculateRemainingCombinations(row, ids)
     #print('comparing, ', result, ' and', answers[i], row, i)
     #assert answers[i] == result
     finalTotal += result
