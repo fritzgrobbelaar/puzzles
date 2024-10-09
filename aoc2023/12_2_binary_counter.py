81647d99ecb799877d8d57eb476903377c3032b5
Fritz Grobbelaar <fritzgrobbelaar@Adaleens-MacBook-Air.local>
2024-10-09 23:07:56 +0100

    Samples pass

--- aoc2023/12_2_binary_counter.py
+++ aoc2023/12_2_binary_counter.py
@@ -3,58 +3,18 @@ listOfText_Puzzle = cleaninput.getfileInputLinesAsList('input12.txt')
 
 
 input_sample = '''???.### 1,1,3
-.??..??...?##. 1,1,3'''.split('\n')
-test1='''?#?#?#?#?#?#?#? 1,3,1,6
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1'''.split('\n')
+
+#input_sample='''?###???????? 3,2,1'''.split('\n')
 
 answers = [1, 4, 1, 1, 4, 10]
 
 rows = input_sample
 #rows= listOfText_Puzzle
-
-# def doesStringMatch(binaryString, originalString):
-#     binary = list(binaryString)
-#
-#     original = list(originalString)
-#     for i, value in enumerate(original):
-#         if value == '?':
-#             pass
-#         elif value == binary[i]:
-#             pass
-#         else:
-#             #print('comparing', ''.join(original), ''.join(binary),value, binary[i],'returning False')
-#             return False
-#     #print('comparing', ''.join(original), ''.join(binary),'returning True')
-#     return True
-#
-# def doesIdsMatch(binaryString, ids):
-#     listBinary = binaryString.split('0')
-#     lengths = [str(len(x)) for x in listBinary if len(x) != 0]
-#     lengthsString = ','.join(lengths)
-#     if lengthsString == ids:
-#         #print('Full match found:', lengthsString, ids, 'True')
-#         return True
-#     if binaryString == '01000100001110':
-#         #print('comparing', lengthsString, ids, 'False')
-#         pass
-#     return False
-#
-#
-# def replaceWithBinary(row, binaryString):
-#     newString = ''
-#     binaryIndex = 0
-#     for value in row:
-#         if value == '?':
-#             value = binaryString[binaryIndex]
-#             binaryIndex += 1
-#         newString = newString + value
-#     return newString
-#
-#
-# assert '000001111' == replaceWithBinary('000??1?11', '001')
-# assert '0100101111' == replaceWithBinary('0?00??1?11', '1101')
 
 def getRemainingStrings(id, row):
     id = int(id)
@@ -63,45 +23,52 @@ def getRemainingStrings(id, row):
     idsAndStrings = []
     length = len(row)
     for i in range(length):
-       # print("entered for-loop", i, length)
-        if '.' != row[i]:
+        print("entered for-loop", i, length)
+        if '.' not in row[i:i+len(searchString)]:
             if len(searchString) + i == length:
-      #          print("Last string matched", i, length, searchString, len(searchString))
+                print("Last string matched", i, length, searchString, len(searchString))
                 if len(row) > id:
                     lookBefore = row[i-1]
                     if lookBefore != '#':
+                        print("appending at the end 2 because good", i + len(searchString))
                         idsAndStrings.append('')
                     else:
                         pass
-     #                   print("Look before disqualifies this ", i)
+                        print("Look before disqualifies this ", i)
                 else:
+                    print("appending at the end")
                     idsAndStrings.append('')
             elif len(searchString)+i < length:
-    #            print('index: ', i+len(searchString) + 1,  row[i + len(searchString)])
-                if row[i + len(searchString)] in ['?', '.']:
-   #                 print("appending", i)
-                    idsAndStrings.append(row[i + len(searchString)+1:])
+                print('index: ', i+len(searchString),  row[i + len(searchString)])
+                if (row[i + len(searchString)] in ['?', '.']):
+                    if i == 0 or (row[i-1] in ['?', '.']):
+                        print("appending", i, 'because ahead is good', row[i + len(searchString)],
+                              'and behind is good',row[i-1], 'remaining row', row[i + len(searchString):])
+                        idsAndStrings.append(row[i + len(searchString)+1:])
     print("returning", idsAndStrings)
     return idsAndStrings
 
-#assert [''] == getRemainingStrings(1, '#')
-#assert [] == getRemainingStrings(1, '##')
-#assert [''] == getRemainingStrings(1, '#?')
-#assert ['', ''] == getRemainingStrings(1, '??')
-#assert [''] == getRemainingStrings(1, '?#')
-#assert ['?', ''] == getRemainingStrings(1, '?.?')
-#assert ['.?','?', ''] == getRemainingStrings(1, '#?.?')
+assert [''] == getRemainingStrings(1, '#')
+assert [] == getRemainingStrings(1, '##')
+assert [''] == getRemainingStrings(1, '#?')
+assert ['', ''] == getRemainingStrings(1, '??')
+assert [''] == getRemainingStrings(1, '?#')
+assert ['?', ''] == getRemainingStrings(1, '?.?')
+assert ['.?',''] == getRemainingStrings(1, '#?.?')
+assert ['..??...?##.', '.??...?##.', '...?##.', '..?##.'] == getRemainingStrings(1, '.??..??...?##.')
+assert [''] == getRemainingStrings(3, '...?##.')
 #print("passed all unit tests")
 
-def calculateRemainingCombinations(remainingRow, remainingIds):
-    print('processing row', remainingRow, remainingIds)
+def calculateRemainingCombinations(row, remainingIds):
+    print('processing row', row, remainingIds)
     consumingID = remainingIds[0]
     newRemainingIds = remainingIds[1:]
-    remainingStrings = getRemainingStrings(consumingID, remainingRow)
+    remainingStrings = getRemainingStrings(consumingID, row)
     count = 0
     for remaining in remainingStrings:
         if len(newRemainingIds) == 0:
-            print("Found a matching one! returning", len(remainingStrings))
+            print("Found no ids remaining! returning", len(remainingStrings), 'string remaining',
+                  remainingStrings, 'started with', row)
             return len(remainingStrings)
         else:
             count += calculateRemainingCombinations(remaining, newRemainingIds)
