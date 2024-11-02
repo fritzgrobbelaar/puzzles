ee145adda41483ea9fed142193fa044c1f6b4c40
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2024-11-02 09:07:07 +0000

    15_1 started

--- aoc2023/14_1_rocks_weight.py
+++ aoc2023/14_1_rocks_weight.py
@@ -2,124 +2,44 @@ import cleaninput
 from datetime import datetime
 from collections import defaultdict
 from itertools import repeat
-listOfText_Puzzle = cleaninput.getfileInputLinesAsList('input14.txt')
+listOfText_Puzzle = cleaninput.getfileInputLinesAsList('input15.txt')
 
 
-input_sample = '''O....#....
-O.OO#....#
-.....##...
-OO.#O....O
-.O.....O#.
-O.#..O.#.#
-..O..#O..O
-.......O..
-#....###..
-#OO..#....'''.split('\n')
+input_sample = '''rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'''.split('\n')
+
 
 input = listOfText_Puzzle
 #input = input_sample
 
-
+input = input[0]
 rows = []
-for row in input:
+for row in input.split(','):
     rows.append(list(row))
 
-def printBlock(block):
-    print()
-    for row in block:
-        print(''.join(row))
 
-def tiltNorth(rows):
-    rows = list(map(list, zip(*rows)))
+def calculateOne(currentValue, newChar):
+    currentValue += ord(newChar)
+    #print(f'{currentValue=} 1')
+    currentValue = currentValue*17
+    #print(f'{currentValue=} position 2')
 
-    for i,row in enumerate(rows):
-        stringRow = ''.join(row)
-        splitRow = stringRow.split('#')
-        sortedShortStrings = []
-        for shortString in splitRow:
-            #print(f'{shortString=} {shortString.count("O")=}  {shortString.count(".")=}')
-            sortedShortStrings.append('O'*shortString.count("O") + '.'*shortString.count("."))
-        rows[i] = list('#'.join(sortedShortStrings)    )
+    currentValue = currentValue % 256
+    #print(f'{currentValue=} 3')
 
-    rows = rows = list(map(list, zip(*rows)))
-    return rows
+    return currentValue
 
-def tiltSouth(rows):
-    rows = list(map(list, zip(*rows)))
+assert calculateOne(0, 'H') == 200
 
+def calculate(row):
+    currentValue = 0
+    for char in row:
+        currentValue = calculateOne(currentValue, char)
+    return currentValue
 
-    for i,row in enumerate(rows):
-        stringRow = ''.join(row)
-        splitRow = stringRow.split('#')
-        sortedShortStrings = []
-        for shortString in splitRow:
-            #print(f'{shortString=} {shortString.count("O")=}  {shortString.count(".")=}')
-            sortedShortStrings.append('.'*shortString.count(".")+'O'*shortString.count("O"))
-        rows[i] = list('#'.join(sortedShortStrings)    )
+assert calculate('HASH') == 52
 
-    rows = rows = list(map(list, zip(*rows)))
-    return rows
+total = 0
+for i, row in enumerate(rows):
+    total += calculate(row)
 
-def tiltEast(rows):
-
-    for i,row in enumerate(rows):
-        stringRow = ''.join(row)
-        splitRow = stringRow.split('#')
-        sortedShortStrings = []
-        for shortString in splitRow:
-           # print(f'{shortString=} {shortString.count("O")=}  {shortString.count(".")=}')
-            sortedShortStrings.append('.'*shortString.count(".")+'O'*shortString.count("O"))
-        rows[i] = list('#'.join(sortedShortStrings)    )
-
-    return rows
-
-def tiltWest(rows):
-
-    for i,row in enumerate(rows):
-        stringRow = ''.join(row)
-        splitRow = stringRow.split('#')
-        sortedShortStrings = []
-        for shortString in splitRow:
-            #print(f'{shortString=} {shortString.count("O")=}  {shortString.count(".")=}')
-            sortedShortStrings.append('O'*shortString.count("O") + '.'*shortString.count("."))
-        rows[i] = list('#'.join(sortedShortStrings)    )
-
-    return rows
-
-storeStates = {}
-frequency = None
-for i in range(200):
-    rows = tiltNorth(rows)
-    rows = tiltWest(rows)
-    rows = tiltSouth(rows)
-    rows = tiltEast(rows)
-    dictKey = tuple([tuple(row) for row in rows])
-    
-    if dictKey not in storeStates:
-        storeStates[dictKey]= []
-    else:
-        print('frequency', i-storeStates[dictKey][0],' at iteration', i)
-        if frequency == None:
-            frequency = i-storeStates[dictKey][0]
-    storeStates[dictKey].append(i)
-    if frequency and (1000_000_000-i-1) % frequency == 0:
-    #if frequency and (20-i) % frequency == 0:
-    
-        print('cycle is in sync at', i)
-        break
-
-print('storeStates',storeStates.values())
-printBlock(rows)
-
-def calculateLoad(rows):
-    rows = rows = list(map(list, zip(*rows)))
-
-    length = len(rows[0])
-    total = 0
-    for i, row in enumerate(rows):
-        for j, value in enumerate(row):
-            if value == 'O':
-                total += length-j
-    return total
-
-print('total', calculateLoad(rows))
+print('total', total)
