c1ad2a52b143a827e3e7f51361e0d505762dde97
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2024-12-02 05:28:41 +0000

    2_2 2024 done

--- aoc2023/2_1_2024.py
+++ aoc2023/2_1_2024.py
@@ -1,46 +1,58 @@
 import cleaninput
+
 listOfText = cleaninput.getfileInputLinesAsList('input2_2024.txt')
 
-sample = '''3   4
-4   3
-2   5
-1   3
-3   9
-3   3'''.split('\n')
+sample = '''7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 2 3 7 8
+1 2 4 7 8 11 14 17
+1 3 6 7 9'''.split('\n')
 
-listOfText= sample
-
-maxCount = {
-    'red':   12,
-    'green': 13,
-    'blue': 14
-}
+#listOfText = sample
 
 total = 0
 for i, row in enumerate(listOfText):
-    row = row.replace('\\n','')
-    print(f'\n#### Processing {row=}')
-    maxBlue = 0
-    maxGreen = 0
-    maxRed = 0
-    gameNumber = int(row.split(':')[0].split(' ')[1])
-    print(f'\n{gameNumber=} {row=}')
+    #print(f'\n#### Processing {row=}')
 
-    numbersFull = row.split(':')[1]
-    numbers = numbersFull.split(';')
-    rowBad = False
-    print('numbers:', numbers)
-    for number in numbers:
+    rowIn = row.split()
+    row = []
+    for value in rowIn:
+        row.append(int(value))
 
-        fewValues = number.split(',')
-        for oneValue in fewValues:
-            oneValue = oneValue[1:]
-            value,key = oneValue.split(' ')
-            if int(value) > maxCount[key.strip()]:
-                print('bad row:', gameNumber, key.strip(), value)
-                rowBad = True
-                break
-    if not rowBad:
-        print('good row:', gameNumber)
-        total = total + gameNumber
-print('total:', total)
+    originalRow = row[:]
+    addOne = False
+    for removeOne in range(len(row)+1):
+        row = originalRow[:]
+        if removeOne != len(row):
+            row.pop(removeOne)
+        #print(f'{row=}')
+        incr = True
+        decr = True
+        prevValue = row[0]
+        #print(f'\n#### Processing {row=}')
+        for value in row[1:]:
+            if (abs(value-prevValue))==0:
+                print('jump too small')
+                incr=False
+                decr=False
+            if (abs(value-prevValue))>3:
+                incr=False
+                decr=False
+            if value > prevValue:
+                decr = False
+            if value < prevValue:
+                incr = False
+            prevValue=value
+        if incr or decr:
+            addOne=True
+
+    if addOne:
+        print(f'Success {originalRow=}')
+        total = total + 1
+    else:
+        print(f'Fail {originalRow=}')
+
+print(f'{total=}')
