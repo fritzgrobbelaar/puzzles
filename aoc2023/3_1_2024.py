335d8e2872c573bad6f63172fa27c61f601a8944
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2024-12-03 05:11:23 +0000

    3_2

--- aoc2023/3_1_2024.py
+++ aoc2023/3_1_2024.py
@@ -2,14 +2,14 @@ import cleaninput, re
 
 listOfText = cleaninput.getfileInputLinesAsList('input3_2024.txt')
 
-sample = '''xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'''.split('\n')
+sample = '''xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))'''.split('\n')
 
 #listOfText = sample
 
-regex = r'mul\(\d+,\d+\)'
+regex = r'(don\'t|do|mul\(\d+,\d+\))'
 
 
-
+do=True
 total = 0
 for i, row in enumerate(listOfText):
     print(f'\n#### Processing {row=}')
@@ -17,10 +17,17 @@ for i, row in enumerate(listOfText):
     allMatches = re.findall(regex, row)
     for match in allMatches:
         print(f'{match=}')
-        match = match.replace('mul(', '')
-        match = match.replace(')', '')
-        matches = match.split(',')
-        print(f'{matches=}')
-        total += int(matches[0])*int(matches[1])
+        if match == "don't":
+            do = False
+            print('switching to dont')
+        elif match == 'do':
+            do = True
+            print('switching to do')
+        elif do:
+            match = match.replace('mul(', '')
+            match = match.replace(')', '')
+            matches = match.split(',')
+            print(f'{matches=}')
+            total += int(matches[0])*int(matches[1])
 
 print(f'{total=}')
