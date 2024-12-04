d1c0feb97778410ea3b0cc7bcb1901f6b3b43731
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2024-12-04 05:48:59 +0000

    Getting wrong answer

--- aoc2023/4_1_2024.py
+++ aoc2023/4_1_2024.py
@@ -2,22 +2,119 @@ import cleaninput, re
 
 listOfText = cleaninput.getfileInputLinesAsList('input4_2024.txt')
 
-sample = '''xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'''.split('\n')
+sample = '''..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....'''.split('\n')
 
-listOfText = sample
+sample2='''MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX'''.split('\n')
 
+listOfText = sample2
 
+def getDiagonal(listOfText):
+    listOfLists = []
+    for row in listOfText:
+        listOfLists.append(list(row))
+    diagonal = []
+    for i, row in enumerate(listOfLists):
+        for j, value in enumerate(row):
+            rowNum = i + j
+            if len(diagonal) <= rowNum:
+                diagonal.append([])
+            diagonal[rowNum].append(value)
+    diagStrings = []
+    for row in diagonal:
+        diagStrings.append(''.join(row))
+    #print(f'{diagStrings=}')
+    return diagStrings
 
+assert getDiagonal(['123',
+'234',
+'345']) == ['1',
+'22',
+'333',
+'44',
+'5']
 
-total = 0
-for i, row in enumerate(listOfText):
-    print(f'\n#### Processing {row=}')
+def printList(listOfText):
+    print('-- ListOfText --')
+    for row in listOfText:
+        print(''.join(row))
 
-        print(f'{match=}')
-        match = match.replace('mul(', '')
-        match = match.replace(')', '')
-        matches = match.split(',')
-        print(f'{matches=}')
-        total += int(matches[0])*int(matches[1])
+def count(listOfText):
+
+    total = 0
+    for row in listOfText:
+        total = total + len(re.findall('XMAS', row))
+    return total
+
+assert 2 == count('''MXMAS
+MMMMXMAS'''.split('\n'))
+
+def getReversed(listOfText):
+    reversed = []
+    for row in listOfText:
+        rev = list(row)
+        rev.reverse()
+        reversed.append(''.join(rev))
+    return reversed
+
+def getTransposed(listOfText):
+    listOfLists = []
+    for row in listOfText:
+        listOfLists.append(row)
+    listOfLists =  list(map(list, zip(*listOfLists)))
+    listOfText = []
+    for row in listOfLists:
+        listOfText.append(''.join(row))
+    return listOfText
+
+print('point 1')
+printList(listOfText)
+total = count(listOfText)
+print(f'{total=}') # 3
+diag = getDiagonal(listOfText)
+printList(diag)
+total += count(diag)
+print(f'{total=}') #4
+
+listOfText = getReversed(listOfText)
+print('\n\n --- point 2') # 6
+printList(listOfText)
+total += count(listOfText)
+print(f'{total=}')
+diag = getDiagonal(listOfText)
+printList(diag)
+total += count(diag)
+print(f'{total=}')
+
+listOfText = getTransposed(listOfText)
+print('\n\n --- point 3')
+printList(listOfText)
+total += count(listOfText)
+print(f'{total=}') # should be 8 - is 8
+diag = getDiagonal(listOfText)
+printList(diag)
+total += count(diag)
+print(f'{total=}') # should be 12 - is 12
+
+listOfText = getReversed(listOfText)
+print('\n\n --- point 4')
+printList(listOfText)
+total += count(listOfText)
+print(f'{total=}') # should be 14 - is 14
+diag = getDiagonal(listOfText)
+printList(diag)
+total += count(diag)
 
 print(f'{total=}')
