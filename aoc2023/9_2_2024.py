2de138153cbc886399341167d011b0a90c733d8d
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2024-12-09 07:53:10 +0000

    9_2 lets try again

--- aoc2023/9_2_2024.py
+++ aoc2023/9_2_2024.py
@@ -7,77 +7,102 @@ listOfText = cleaninput.getfileInputLinesAsList('input9_2024.txt')
 
 sample = '''2333133121414131402'''.split('\n')
 #sample = '''12345'''.split('\n')
+#sample = '''432111'''.split('\n')
 
-#listOfText = sample
+listOfText = sample
 
 line = listOfText[0]
 lineList = list(line)
 
 newLine = []
 emptySizeIndex = []
-discSizeIndex = []
+unProcessedDiscUsageIndex = []
 for i, value in enumerate(lineList):
     if i%2 == 0:
-        discSizeIndex.append((len(newLine), int(value),int(i/2)))
+        unProcessedDiscUsageIndex.append({'index': len(newLine), 'size': int(value), 'name': int(i / 2)})
         newLine.extend([str(int(i/2))]*int(value))
     else:
-        emptySizeIndex.append([len(newLine), int(value)])
+        emptySizeIndex.append( {'index':len(newLine), 'size':int(value)})
         #emptySizeIndex.append((len(newLine), int(value)))
         newLine.extend(['.']*int(value))
 
 
 lineList = newLine
 
-print(f'{''.join(lineList)} {emptySizeIndex=} {discSizeIndex=}')
+print(f'{''.join(lineList)} {emptySizeIndex=} {unProcessedDiscUsageIndex=}')
 
-emI = 0
 print(f'{len(lineList)=}')
 print(f'Fixed up {emptySizeIndex=}')
-print(f'{discSizeIndex=}')
+print(f'{unProcessedDiscUsageIndex=}')
 print(f'{''.join(newLine)}')
-newDiscSizeIndex = copy.deepcopy(discSizeIndex)
-for valI in range(len(discSizeIndex)-1,0,-1):
-    #print(f'{valI=} {emI=}, {''.join(lineList)=}')
-    value = discSizeIndex[valI]
-    emI = 0
-    while (emI < len(emptySizeIndex)-1) and (emptySizeIndex[emI][1] < value[1]):
-     #   print(f'{emI=} did not find space large enough {emptySizeIndex[emI][1]=} in small space available {value[1]=}')
-        emI += 1
+upToDateDiscUsageIndex = copy.deepcopy(unProcessedDiscUsageIndex)
+for valueListIndex in range(len(unProcessedDiscUsageIndex) - 1, 0, -1):
+    value = unProcessedDiscUsageIndex[valueListIndex]
 
-    if emI == len(emptySizeIndex)-1:
-        #print('Ran out of empty space for indexed item', emI)
+    print(f'\nBefore iteration of {value=} {upToDateDiscUsageIndex=}')
+    print(f'Before iteration of {value=}  {emptySizeIndex=}')
+    print(f'Before iteration of {value=} {unProcessedDiscUsageIndex=}')
+
+    valueLocationIndex = value['index']
+    valueSize = value['size']
+    valueFileIndex = value['name']
+    # print(f'Processing {value=} {valueListIndex=} {valueLocationIndex=}  {valueSize=} {valueFileIndex=} {''.join(lineList)=}')
+    emptySpaceLocationIndex = None
+    emptySpaceListIndex = 0
+    print(f' {emptySizeIndex=} {emptySpaceListIndex=} {emptySizeIndex[emptySpaceListIndex]=}')
+    emptySpaceSize = emptySizeIndex[emptySpaceListIndex]['size']
+    while (emptySpaceListIndex < len(emptySizeIndex)-1) and (emptySpaceSize < valueSize):
+     #   print(f'{emI=} did not find space large enough {emptySizeIndex[emI][1]=} in small space available {value[1]=}')
+     emptySpaceListIndex += 1
+     emptySpaceSize = emptySizeIndex[emptySpaceListIndex]['size']
+
+    if emptySpaceListIndex == len(emptySizeIndex)-1:
+        # print('Ran out of empty space for indexed item', emptySpaceListIndex)
         continue
 
     #print(f'Found index of first empty space big enough index {emptySizeIndex[emI][0]=}, space size {emptySizeIndex[emI][1]=}')
-    if valI < emI:
-        #print(f'Continue {emI=} {valI=}')
+    if valueLocationIndex < emptySpaceListIndex:
+        # print(f'Continue as we cannot move items at  {valueLocationIndex=} to empty space with larger index {emptySpaceListIndex=}')
         continue
- #   print(f'Swopping {emI=} {valI=} {lineList[emI]=} {lineList[valI]=}')
-    for i in range(value[1]):
-        lineList[emptySizeIndex[emI][0]+i] = lineList[discSizeIndex[valI][0]+i]
-        lineList[discSizeIndex[valI][0]+i] = '.'
-    originalNewIndex = emptySizeIndex[emI][0]
-    emptySizeIndex[emI][1] = emptySizeIndex[emI][1] - value[1]
-    emptySizeIndex[emI][0] = emptySizeIndex[emI][1] + value[1]+1
+    emptySpaceLocationIndex = emptySizeIndex[emptySpaceListIndex]['index']
+    print(f'Swopping {emptySpaceLocationIndex=} {valueLocationIndex=}')
+    for i in range(valueSize):
+        # print(f'{emptySpaceLocationIndex+1=} {valueLocationIndex+1=}')
+        lineList[emptySpaceLocationIndex+i] = lineList[valueLocationIndex+i]
+        lineList[valueLocationIndex+i] = '.'
 
-    movedItem = discSizeIndex.pop(valI)
+    #print(f'line list', ''.join(lineList))
 
-    newDiscSizeIndex.remove(movedItem)
+    # print(f'Before update {emptySizeIndex=}')
+    originalNewIndex = emptySizeIndex[emptySpaceListIndex]['index']
+    emptySizeIndex[emptySpaceListIndex]['size'] = emptySizeIndex[emptySpaceListIndex]['size'] - valueSize
+    emptySizeIndex[emptySpaceListIndex]['index'] = emptySizeIndex[emptySpaceListIndex]['index'] + valueSize
 
-    #print(f'Before the move {newDiscSizeIndex=}')
-    for m, valueNewDisc in enumerate(newDiscSizeIndex):
+    # print(f'After update {emptySizeIndex=}')
 
-        #print(f'Comparing {originalNewIndex=} with {valueNewDisc[0]=}')
-        if originalNewIndex < valueNewDisc[0]:
+    movedItem = unProcessedDiscUsageIndex.pop(valueListIndex)
+
+    upToDateDiscUsageIndex.remove(movedItem)
+
+    print(f'Before the move {upToDateDiscUsageIndex=} {movedItem=}')
+    for m, valueNewDisc in enumerate(upToDateDiscUsageIndex):
+
+        print(f'Comparing {originalNewIndex=} with {valueNewDisc['index']=}')
+        if originalNewIndex < valueNewDisc['index']:
             #print(f'Moving to index {originalNewIndex=} {m=} {movedItem=}', (originalNewIndex), movedItem[1], movedItem[2])
-            movedItem = (originalNewIndex), movedItem[1], movedItem[2]
-            newDiscSizeIndex = newDiscSizeIndex[:m] + [movedItem] + newDiscSizeIndex[m:]
+            movedItem = {'index':originalNewIndex, 'size':movedItem['size'], 'name':movedItem['name']}
+            upToDateDiscUsageIndex = upToDateDiscUsageIndex[:m] + [movedItem] + upToDateDiscUsageIndex[m:]
             break
-    # print(f'After the move {newDiscSizeIndex=}')
-    # print(f'Fixed up {emptySizeIndex=}')
-    # print(f'Fixed up {discSizeIndex=}')
-    #
-    # print(f'{''.join(newLine)}')
+    else:
+        print('item never moved - lets add it to the end')
+        movedItem = {'index':originalNewIndex, 'size':movedItem['size'], 'name':movedItem['name']}
+        upToDateDiscUsageIndex = upToDateDiscUsageIndex + [movedItem]
+
+    print(f'After iteration of {value=} {upToDateDiscUsageIndex=}')
+    print(f'After iteration of {value=}  {emptySizeIndex=}')
+    print(f'After iteration of {value=} {unProcessedDiscUsageIndex=}')
+
+    print(f'{''.join(newLine)}')
 
 
 def getChecksum(lineList):
@@ -90,16 +115,17 @@ def getChecksum(lineList):
 def getChecksum2(newDiscSizeIndex):
     total = 0
     for disc in newDiscSizeIndex:
-        start = disc[0]
-        size = disc[1]
-        id = disc[2]
-
+        start = disc['index']
+        size = disc['size']
+        id = disc['name']
         for i in range(size):
             total += (start+i)*int(id)
     return total
 
-print('checksum2', getChecksum2(newDiscSizeIndex))
-print('checksum', getChecksum(lineList))
+print(f'checksum2 {upToDateDiscUsageIndex=}', getChecksum2(upToDateDiscUsageIndex))
+print(f'checksum {''.join(lineList)=}', getChecksum(lineList))
 
 # too low 95479
 #also too low 6333626232522
+#also too low 6333644568623
+#what about   8478907259440 # also bad - can retry at 07:24
