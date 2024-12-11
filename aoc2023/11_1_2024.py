34656f30df4061bf408a551e6a810c6f108a1fba
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2024-12-11 05:56:00 +0000

    11_1 fast

--- aoc2023/11_1_2024.py
+++ aoc2023/11_1_2024.py
@@ -1,9 +1,7 @@
 import cleaninput
 from datetime import datetime
 from functools import cmp_to_key
-from collections import deque
-import itertools
-
+import datetime
 import copy
 listOfText = cleaninput.getfileInputLinesAsList('input11_2024.txt')
 
@@ -12,7 +10,7 @@ listOfText = cleaninput.getfileInputLinesAsList('input11_2024.txt')
 sample = '''125 17'''.split('\n')
 #sample = '''0 1 10 99 999'''.split('\n')
 
-listOfText = sample
+#listOfText = sample
 
 listOfLists = []
 total = 0
@@ -22,7 +20,7 @@ for row in listOfText:
 stones = listOfLists[0]
 
 stones = [int(value) for value in stones]
-stones = deque(stones)
+#stones = [[stone] for stone in stones]
 
 rules = {
     '0': '1',
@@ -30,35 +28,45 @@ rules = {
     'else': '*2024'
 }
 
+
 def blink(stones):
     #print(f'\n Before iteration {stones=}')
-    stonesOriginal = copy.deepcopy(stones)
-    arrayJump = 0
-    for i,value in enumerate(stonesOriginal):
-        i = i + arrayJump
-        #print(f'Processing {value=}')
-        if value == 0:
-            valueArray = deque([1])
+    #stonesOriginal = copy.deepcopy(stones)
+
+    for i,value in enumerate(stones):
+    #    print(f'Processing {value=}')
+        if type(value) == list:
+            valueArray = blink(value)
+        elif value == 0:
+            valueArray = [1]
         elif len(str(value)) %2 == 0:
             valueString = str(value)
             arrayLengthHalf = int(len(valueString)/2)
-            valueArray = deque([int(valueString[:arrayLengthHalf]), int(valueString[arrayLengthHalf:])])
-            arrayJump += 1
+            valueArray = [int(valueString[:arrayLengthHalf]), int(valueString[arrayLengthHalf:])]
         else:
-            valueArray = deque([value *2024])
-        #print(f'{stones=} {i=}')
-        #print(f'{itertools.islice(stones,0,i)=} + {valueArray=} + {itertools.islice(stones,i+1,len(stones))=} {i=}')
-        stones = deque(itertools.islice(stones,0,i)) + valueArray + deque(itertools.islice(stones,i+1,len(stones)))
-
-        #print(f'After iteration {stones=}')
+            valueArray = [value *2024]
+      #  print(f'{stones=} {i=}')
+        stones[i] = valueArray
+     #   print(f'After iteration {stones=}')
     #print(f'After blink {stones=}')
     return stones
 
-#assert [253000, 1, 7] == blink(stones)
+#assert [[253000], [1, 7]] == blink(stones)
+
+def count(stones):
+    total = 0
+    for stoneList in stones:
+        if type(stoneList)== list:
+            total += count(stoneList)
+        else:
+            total += 1
+    return total
 
 for i in range(25):
-    print('Processing iteration', i)
+    print('Processing iteration', i+1, datetime.datetime.now())
     stones = blink(stones)
 
-print(f'{stones=}')
-print(f'{len(stones)=}')
+total = count(stones)
+
+#print(f'{stones=}')
+print(f'{total=}')
