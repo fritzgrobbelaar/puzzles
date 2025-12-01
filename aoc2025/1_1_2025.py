79642ad09ecc8fde27b7cc8e81e2bd586487df92
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2025-12-01 07:52:32 +0000

    sample passes

---
+++ aoc2025/1_1_2025.py
@@ -0,0 +1,40 @@
+sampleText = '''L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82'''
+#response = requests.get('https://adventofcode.com/2025/day/1/input')
+with open('input_1.txt', 'r') as handle:
+ text = handle.read()
+
+#text = sampleText
+rows = text.split('\n')
+
+position = 50
+size = 100
+counter = 0
+previousDirection = 'O'
+for i, row in enumerate(rows):
+    if row.strip() == '':
+        continue
+    #print(f'{row=}')
+    direction = row[0]
+    distance = int(row[1:])
+    if position == 0:
+        if (direction == 'L') or (previousDirection == 'L'):
+            counter +=1
+            #print('increase counter')
+    if direction == 'L':
+        position = position - distance
+    else:
+        position = position + distance
+
+    position = position % 100
+    previousDirection = direction
+print(f'{position=} after {row=} {counter=} {i=}')
+#822 is too low
