963765959d368b708ee46c49ba81b16798f4af7d
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2025-12-02 06:10:57 +0000

    2_1 passed

---
+++ aoc2025/2_1_2025.py
@@ -0,0 +1,55 @@
+sampleText = '''11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124'''
+
+#response = requests.get('https://adventofcode.com/2025/day/1/input')
+with open('input_2.txt', 'r') as handle:
+ text = handle.read()
+
+#comment to run
+#text = sampleText
+rows = text.split(',')
+
+def getSumInvalidIDs(start, end):
+    print(f'Starting {start=} {end=}')
+    end = end + 1
+    total = 0
+    if len(str(start)) == len(str(end)):
+        if len(str(start)) % 2 == 1:
+            print(f'Unequal length - so no duplicate IDs found')
+            return total
+    for i in range(start, end):
+        i_str = str(i)
+        i_len = len(i_str)
+        #print(f' {i=}')
+        if i_len % 2 == 1:
+            continue
+        i_half = int(i_len/2)
+        #print(f'{i_half=}')
+        if i_str[:i_half] == i_str[i_half:]:
+            #print(f'Found something: {total=}')
+            total += i
+    print(f'Returning something: {total=}')
+    return total
+
+assert 33 == getSumInvalidIDs(11, 22)
+assert 99 == getSumInvalidIDs(95, 115)
+assert 1010 == getSumInvalidIDs(998, 1012)
+assert 1188511885 == getSumInvalidIDs(1188511880,1188511890)
+assert 222222 == getSumInvalidIDs(222220,222224)
+
+assert 0 == getSumInvalidIDs(1698522,1698528)
+assert 446446 == getSumInvalidIDs(446443,446449)
+assert 38593859 == getSumInvalidIDs(38593856,38593862)
+assert 0 == getSumInvalidIDs(565653,565659)
+assert 0 == getSumInvalidIDs(824824821,824824827)
+assert 0 == getSumInvalidIDs(2121212118,2121212124)
+
+assert 11 == getSumInvalidIDs(11,12)
+assert 11 == getSumInvalidIDs(10,11)
+assert 11 == getSumInvalidIDs(11,11)
+
+bigTotal = 0
+for row in rows:
+    row = row.split('-')
+    print(f'{row=}')
+    bigTotal += getSumInvalidIDs(int(row[0]), int(row[1]))
+print(f'{bigTotal=}')
