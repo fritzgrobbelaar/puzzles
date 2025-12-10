62b722523597b4769828f6188c6cf4cfd6b2e15b
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2025-12-10 20:22:32 +0000

    10_2 test cases pass

--- aoc2025/10_2.py
+++ aoc2025/10_2.py
@@ -6,9 +6,9 @@ import itertools
 global listOfText
 listOfText = cleaninput.getfileInputLinesAsList('input_10.txt')
 
-sample='''[.##.] (1) {0,2,0,0}
-[.##.] (1) (2,1) {0,2,0,0}
-'''.split('\n')
+sample='''[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}'''.split('\n')
 
 extracases = '''
 [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
@@ -35,7 +35,7 @@ def sortSwitches(switches):
     
     return switches
     
-assert ['(1,2,3,4)', '(0,2,3,4)', '(0,1,2)', '(2,3)', '(0,4)'] == sortSwitches(['(0,2,3,4)' ,'(2,3)', '(0,4)' ,'(0,1,2)' ,'(1,2,3,4)'])
+assert ['(0,4)', '(2,3)',  '(0,1,2)','(0,2,3,4)',  '(1,2,3,4)',] == sortSwitches(['(0,2,3,4)' ,'(2,3)', '(0,4)' ,'(0,1,2)' ,'(1,2,3,4)'])
 
 def parseSwitches(listOfStrings):
     listOfTuples = []
@@ -90,42 +90,45 @@ assert 'too high' == compareState(endState=(2,3), state=(3,1))
 assert 'too low' == compareState(endState=(2,3), state=(1,2))
 assert 'too low' == compareState(endState=(2,3), state=(2,2))
 
-def toDelete_calculateNewState(state, iteration, switches, switchesCounters):
+def calculateNewState(state, switchesCounters, switches):
+   # print(f'\n{switchesCounters=}')
     #print(f'\nCalculate new state from: {state=} {iteration=} {switches=}')
-    for i, it in enumerate(iteration):
+    for i, it in enumerate(switchesCounters):
         if it == 0:
             continue
         switch = switches[i]
-        switchesCounters[i] += it
         for j,value in enumerate(state):
             state[j] += it*switch[j]
     #print(f'Returning {state=} {iteration=}, {switchesCounters=}')
-    return state, switchesCounters
+    return state
 
 #assert str(([24,56,45], [1])) ==  #str(calculateNewState([23,56,45], [1], (1,0,0)], switchesCounters = [0]))
 #assert str(([22,56,45], [2])) ==  str(calculateNewState([23,56,45], [-1], [(1,0,0)], switchesCounters = [3]))
 #assert str(([24, 56, 45],  [11,0])) ==  str(calculateNewState([23,56,45], [1,0], [(1,0,0), (1,1,0)], switchesCounters = [10,0]))
 #assert str(([23, 55, 0], [5,7,6])) ==  str(calculateNewState([23,56,0], [0,1,-1], [(1,0,1),(1,0,0), (1,1,0)], switchesCounters = [5,6,7]))
 
-def toDelete_calculateNextIteration(iteration, state, lastResult, switchesCounters):
-    print(f'\n--calculateNextIteration: {iteration=}, {state=}, {lastResult=}, {switchesCounters=}')
-    if lastResult == 'too low': # no change to the iteration
-        iteration = iteration = [iterValue if iterValue != -1 else 0 for iterValue in iteration]
-    elif lastResult == 'matched':
-        raise Exception('could have been done already')
-    else:  # careful binary logic
-        for i, value in enumerate(iteration):
-            if value == 1:
-                iteration[i] = -1
-                iteration[i+1] = 1
-                break
-            else:
-                iteration[i] = 0
-    print(f'returning {iteration=}')
-    return iteration
-                
-#assert [1,0,0] == calculateNextIteration([1,0,0], [23,56,45], 'too low', [0,0,0])
-#assert [-1,1,0] == calculateNextIteration([1,0,0], [11,5,5], 'too high', [1,0,0])
+def tensIncrease(switchesCounters):
+    #print(f'Increasing {switchesCounters=} by tens')
+    length = len(switchesCounters)
+    value = 0
+    for i in range(length-1, -1, -1):
+        value = switchesCounters[i]
+     #   print(f'Checking position {i} with value {value} {switchesCounters=}')
+        if value == 0:
+            pass
+        else:
+            if i == 0:
+                raise Exception('bad things')
+            #print(f'adding to position {i=}')
+            switchesCounters[i-1] += 1
+            switchesCounters[i] = 0
+            break
+    #print(f'Returning {switchesCounters=}')
+    return switchesCounters
+assert [1,1,0] == tensIncrease([1,0,1])
+assert [2,0,0] == tensIncrease([1,1,0])
+#assert 'bad things' == tensIncrease([1,0,0])
+
 
 def getLowestIteration(row):
     print('\n\n ---- New row --',row)
@@ -137,6 +140,7 @@ def getLowestIteration(row):
     switches = parseSwitches(switches)
     length = len(endState) 
     state = [0]*length
+    originalState = state[:]
     switches = convertSwitches(switches, state[:])
     switchesCounters = [0]*len(switches)
     print(f'{switches=}')
@@ -144,28 +148,28 @@ def getLowestIteration(row):
     
     lastResult = 'too low'
     while lastResult != 'matched':
-       # print(f'{iteration=} {state=} {lastResult=}')
-        
-        
+       # print(f'{switchesCounters=} {state=} {lastResult=}')
         lastResult = compareState(endState,state)
         if lastResult == 'matched':
-            break
+            resultCounter = sum(switchesCounters)
+            print(f'Returning {resultCounter=}')
+            return resultCounter
         if lastResult == 'too low':
+        #    print(f'Too low - increasing last switch {switchesCounters} to {switchesCounters[-1]+1}')
             switchesCounters[-1] += 1
-        else:
+        elif lastResult == 'too high':
             switchesCounters = tensIncrease(switchesCounters)
-    return counter
+        state = calculateNewState(originalState[:], switchesCounters, switches)
+    raise Exception('should not reach here')
 
 print('\n-------------------------\n')
-assert 2 == getLowestIteration(row=['[.##.]', '(1)', '(2,1)', '{0,2,0,0}'])
-
-assert 2 == getLowestIteration(row=['[.##.]', '(1)', '(2,1)', '(3,2,1)', '{0,2,0,0}'])
 assert 1 == getLowestIteration(row=['[.##.]', '(1)', '(2,1)', '(1,2,3)', '{0,1,1,1}'])
+assert 2 == getLowestIteration(row=['[.##.]', '(1)', '(2,1)', '{0,2,0,0}'])
+assert 2 == getLowestIteration(row=['[.##.]', '(1)', '(2,1)', '(3,2,1)', '{0,2,0,0}'])
 assert 3 == getLowestIteration(row=['[.##.]', '(2)', '(1,2)', '(3,2,1)', '{0,2,3,0}'])
 assert 2 == getLowestIteration(row=['[.##.]', '(1)', '{0,2,0,0}'])
-
 assert 10 == getLowestIteration(row=['[.##.]', '(3)', '(1,3)', '(2)', '(2,3)', '(0,2)', '(0,1)', '{3,5,4,7}'])
-
+print('\n-------------------------\n\n\n\n -- the real deal ----\n')
 
 totalCounter = 0
 for row in listOfText:
