f984924b485a4f061e96e7dde0f1f1c0412787f8
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2025-12-10 06:56:54 +0000

    Test cases pass - 10_1

--- aoc2025/10_1.py
+++ aoc2025/10_1.py
@@ -11,72 +11,80 @@ sample='''[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
 '''.split('\n')
 
-test=True
+test=False
 if test:
     listOfText = sample
 print(f'{listOfText=}\n')
-
+listOfText = [item for item in listOfText if item.strip() != '']
 listOfText = [row.split(' ') for row in listOfText]
 
 def convertValueToBinary(value, length):
-    print(f'converting to binary {value=} {length=}')
+    #print(f'converting to binary {value=} {length=}')
     value = value[1:-1]
     value= value.split(',')
     longestString = ['.']*length
     for digit in value:
-        longestString[int(digit)] = '#'
+        try:
+            longestString[int(digit)] = '#'
+        except IndexError:
+            print('something went wrong',length, value)
+            raise
     string = ''.join(longestString)
-
+    #print(f'returning {string=}')
     return string
 
 assert  '...#' == convertValueToBinary('(3)',4)
-assert  '0b10' == convertValueToBinary('(3)',5)
-assert  '0b101' == convertValueToBinary('(2,0)',3)
-assert  '0b101' == convertValueToBinary('(3,1)',4)
+assert  '...#.' == convertValueToBinary('(3)',5)
+assert  '#.#' == convertValueToBinary('(2,0)',3)
+assert  '.#.#' == convertValueToBinary('(3,1)',4)
 
 def convertEndState(endState):
-    print(f'{endState=}')
     endState = endState[1:-1]
-    endState = endState.replace('.','0').replace('#','1')
-    endState = '0b' + endState\
-
-    endState = bin(int(endState, 2))
-    print(f'Returning {endState=}')
     return endState
 
-assert '0b100' == convertEndState('[.#..]')
+assert '.#..' == convertEndState('[.#..]')
+
+def flipSwitches(switches, flips):
+    if len(switches) != len(flips):
+        raise ValueError(' lengths should be the same')
+    newList = []
+    for i,value in enumerate(switches):
+        value2 = flips[i]
+        if value == value2:
+            newList.append('.')
+        else:
+            newList.append('#')
+    return ''.join(newList)
+
+
+assert '....' == flipSwitches('....','....')
+assert '..#.' == flipSwitches('..#.','....')
+assert '..#.' == flipSwitches('....','..#.')
+assert '#..#' == flipSwitches('...#','#...')
+assert '#..#' == flipSwitches('.#.#','##..')
+
+fewestPressesTotal = 0
 
 for row in listOfText:
-    endState = row[0]
+    endState = convertEndState(row[0])
     switches = row[1:-1]
 
     combs = [list(x) for x in itertools.permutations(switches, len(switches))]
     combs = [com for com in combs if len(com) == len(switches)]
     print(f'{len(combs)=}')
+    fewestPresses = 100000
     for comb in combs:
-        for value in comb:
-            convertValueToBinary(value,len(switches)-2)
-        break
-    break
-
-exit()
-from itertools import permutations
-
-my_list = ['A', 'B', 'C']
-all_perms = []
-for r in range(1, len(my_list) + 1):
-    for perm in permutations(my_list, r):
-        all_perms.append(perm)
-
-print(all_perms)
-
-a="0011101"
-b="1011100"
-
-#print('{y=int(a,2) ^ int(b,2)}'.format(a))
-print('a {}'.format(a))
-print('b {}'.format(b))
-y=int(a,2) ^ int(b,2)
-print('y {0:b}'.format(y))
-#drawScaledArea(listOfText, scale)
-print('- the end')
+        state = '.'*len(endState)
+        for i,value in enumerate(comb):
+            if i+1 == fewestPresses:
+                #print('already reached')
+                break
+            flips = convertValueToBinary(value,len(endState))
+            state = flipSwitches(state, flips)
+            #print(f'comparing {state=} {endState=}')
+            if state == endState:
+                print(f'found one - breaking {comb=}')
+                fewestPresses = i+1
+    print(f'{fewestPresses=} for {row=}')
+    fewestPressesTotal += fewestPresses
+print(f'{fewestPressesTotal=}')
