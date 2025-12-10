46bf9721209c4efd96a44b763376a9476865e878
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2025-12-10 07:05:29 +0000

    Test case too slow
    reset

--- aoc2025/10_1.py
+++ aoc2025/10_1.py
@@ -9,9 +9,11 @@ listOfText = cleaninput.getfileInputLinesAsList('input_10.txt')
 sample='''[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+[...#..#.#.] (0,3,8) (4,6,7,9) (1,2,4,5,6,7) (1,2,3,5,6) (7) (0,2,4,5,7) (1,2,5,6,9) (1,2,5,6,7,8,9) (6,9) (2,3,5,8,9) (0,1,2,5,8,9) (0,1,5) (4,9) {48,42,54,27,48,66,42,67,50,68}
+
 '''.split('\n')
 
-test=False
+test=True
 if test:
     listOfText = sample
 print(f'{listOfText=}\n')
@@ -68,6 +70,7 @@ fewestPressesTotal = 0
 for row in listOfText:
     endState = convertEndState(row[0])
     switches = row[1:-1]
+    print(f'{len(switches)=}')
 
     combs = [list(x) for x in itertools.permutations(switches, len(switches))]
     combs = [com for com in combs if len(com) == len(switches)]
